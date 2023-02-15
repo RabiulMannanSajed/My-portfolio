c8bf4a1bfe13ce024b62a19484465d8bbfb97131
RabiiulMannanSajed <msajed211086@bscse.uiu.ac.bd>
2023-02-16 01:22:34 +0600

    My protfolio

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>You can fuck her</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Sharjil love nipa</h3>
       </header>
     </div>
   );
